20297e079f4ce5a8729f2d9d415a48ab5ef2b596
Edie1995 <krukowskaedyta@gmail.com>
2020-01-20 17:44:35 +0000

    Create InstitutionDTO.java

---
+++ src/main/java/pl/kruko/PracaInz/dataTransferObjects/InstitutionDTO.java
@@ -0,0 +1,107 @@
+package pl.kruko.PracaInz.dataTransferObjects;
+
+import java.util.List;
+
+import pl.kruko.PracaInz.models.Status;
+
+public class InstitutionDTO {
+
+	private Long id;
+	private String name;
+	private String city;
+	private String adress;
+	private Long phoneNumber;
+	private Status status;
+	private List<DoctorDTO> doctors;
+
+	public InstitutionDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public Long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(Long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public List<DoctorDTO> getDoctors() {
+		return doctors;
+	}
+
+	public void setDoctors(List<DoctorDTO> doctors) {
+		this.doctors = doctors;
+	}
+
+	@Override
+	public String toString() {
+		return "id=" + id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		InstitutionDTO other = (InstitutionDTO) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
